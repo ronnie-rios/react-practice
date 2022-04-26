0fbb33978241c765af5831e72a8fc4b95e295b82
Ronnie Rios <ronniesmail14@gmail.com>
2022-04-26 11:04:55 -0500

    use ref to handle memory leak

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,17 +4,24 @@ function ToDo() {
     const [loading, setLoading] = useState(true)
 
     const [todo, setTodo] = useState({})
+    const isMounted = useRef(true)
 
     useEffect(()=> {
         fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then((res) => res.json())
         .then((data) => {
-            setTimeout(() => {
+            if(isMounted.current) {
+               setTimeout(() => {
                 setTodo(data)
                 setLoading(false)
-            }, 3000)
+            }, 3000) 
+            }
+            
         })
-    }, [])
+        return () =>{
+            isMounted.current = false
+        }
+    }, [isMounted])
 
     return (
     <div>
