6963a9e0d8a4d0913ae3855dfd3ea3cac662a427
Ronnie Rios <ronniesmail14@gmail.com>
2022-05-02 09:41:27 -0500

    class state prac

--- src/components/ClassUsers.js
+++ src/components/ClassUsers.js
@@ -1,6 +1,18 @@
 import { Component, useState } from "react";
 
 class ClassUsers extends Component{
+    constructor() {
+        this.state ={
+            //all state goes in here, no separate state
+            stateExamples: 'test',
+            arrState: [],
+        }
+    }
+
+    togglestate() {
+        this.setState({})
+    }
+
     render() {
         return(
             <div>
