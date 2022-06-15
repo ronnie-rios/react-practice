a96a05c66d1854fb1d28a1407999b60576ca2964
Ronnie Rios <ronniesmail14@gmail.com>
2022-06-15 09:12:58 -0500

    validation prac spreading state

--- src/components/MiniProjState/Users/AddUser.jsx
+++ src/components/MiniProjState/Users/AddUser.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react'
+import Card from '../UI/Card'
 
 export default function AddUser() {
     const [userData, setUserdata] = useState()
 
     const changeHandler = (e) => {
+        //name of the input
         const name = e.target.name
-        const value = e.target.value
-
+        //default value from the dom
+        const value = e.target.value;
+        //set to the setter, spread both into an obj with name and value
         setUserdata(userData => ({ ...userData, [name]: value }))
     }
 
     const submitHandler = (e) => {
         e.preventDefault()
+        if (userData.username.trim().length === 0 || userData.animal.trim().length === 0) {
+            return
+        }
+        console.log(userData);
+        setUserdata()
     }
     
     return (
     <div>
+        <Card>
         <h3>state mini prac</h3>
         <form onSubmit={submitHandler}>
             <label htmlFor='username'>Username</label>
@@ -24,18 +33,17 @@ export default function AddUser() {
                 name='username' 
                 placeholder='enter your username'
                 onChange={changeHandler}
-                value='name'
             />
             <label htmlFor='favorite-animal'>Favroite animal</label>
             <input 
                 type='text' 
                 name='animal' 
                 placeholder='enter your favorite animal'
-                onChange={changeHandler}
-                value='animal'
+                onChange={changeHandler}     
             />
             <button type='submit'>save your info</button>
         </form>
+        </Card>
     </div>
   )
 }
