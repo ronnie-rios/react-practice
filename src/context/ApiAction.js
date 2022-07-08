ea493f716953fddeba85a50beca3dad022b268e9
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-08 08:27:54 -0500

    action created for fetch calls

---
+++ src/context/ApiAction.js
@@ -0,0 +1,13 @@
+const url = 'https://jsonplaceholder.typicode.com/'
+
+export const fetchAllPosts = async () => {
+    const response = await fetch(`${url}/posts`)
+    const data = await response.json
+    return data
+}
+
+export const fetchSinglePost = async (id) => {
+    const response = await fetch(`${url}/posts/${id}`)
+    const data = await response.json
+    return data
+}
