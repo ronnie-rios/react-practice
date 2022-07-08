ca9cc4362a7a4ceaadff47a6ef7de9da3723cc1f
Ronnie Rios <ronniesmail14@gmail.com>
2022-07-08 08:24:19 -0500

    reducer created

--- src/context/ApiReducer.js
+++ src/context/ApiReducer.js
@@ -1,5 +1,35 @@
 //create reducer
 
 const apiReducer = (state, action) => {
-    
+    //reducer takes in two arguemnts, state, action
+    switch (action.type) {
+        //when this action gets dispatched will return that state
+        case 'GET_POSTS':
+            return {
+                ...state,
+                posts: action.payload,
+                loading: false
+            }
+        case 'GET_SINGLE_POST':
+            return {
+                ...state,
+                post: action.payload,
+                loading: false
+            }
+        case 'GET_COMMENTS':
+            return {
+                ...state,
+                comments: action.payload,
+                loading: false
+            }
+        case 'SET_LOADING':
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state
+
+    }
 }
+export default apiReducer
