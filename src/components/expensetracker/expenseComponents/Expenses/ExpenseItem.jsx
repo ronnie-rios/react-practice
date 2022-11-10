89c06326a4ffb39fb7a197febe3d719b8a7ffd1d
Ronnie Rios <ronniesmail14@gmail.com>
2022-11-10 08:40:21 -0600

    semantic tag replace li for div

--- src/components/expensetracker/expenseComponents/Expenses/ExpenseItem.jsx
+++ src/components/expensetracker/expenseComponents/Expenses/ExpenseItem.jsx
@@ -10,15 +10,17 @@ function ExpenseItem(props) {
     console.log('click!!!')
   }
   return (
-    <Card className='expense-item'>
-      {props.expenses.map(item => (
-        <div id={item.id} className='expense-item__description'>
-            <h2>expenses: {item.title}{title}</h2>
-            <div className='expense-item__price'>{item.amount}</div>
-            <button onClick={clickHandler}>zClick!</button>
-        </div>
-      ))}
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        {props.expenses.map(item => (
+          <div id={item.id} className='expense-item__description'>
+              <h2>expenses: {item.title}{title}</h2>
+              <div className='expense-item__price'>{item.amount}</div>
+              <button onClick={clickHandler}>zClick!</button>
+          </div>
+        ))}
+      </Card>
+    </li>
   )
 }
 
