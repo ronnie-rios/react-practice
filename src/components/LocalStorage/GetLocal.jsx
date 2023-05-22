9761c08a11d6ca486309a77c58f5509a446b75aa
Ronnie Rios <ronniesmail14@gmail.com>
2023-05-22 18:31:31 -0500

    react local storage

---
+++ src/components/LocalStorage/GetLocal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetLocal = () => {
+  return (
+    <div>GetLocal</div>
+  )
+}
+
+export default GetLocal
---
+++ src/components/LocalStorage/PostLocal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostLocal = () => {
+  return (
+    <div>PostLocal</div>
+  )
+}
+
+export default PostLocal
