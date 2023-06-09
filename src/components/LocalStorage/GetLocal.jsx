c9e65c748a2971fd1929c0a9b27dcb276f9d6a4d
Ronnie Rios <ronniesmail14@gmail.com>
2023-06-09 14:13:25 -0500

    storage

--- src/components/LocalStorage/GetLocal.jsx
+++ src/components/LocalStorage/GetLocal.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 
 const GetLocal = () => {
-    const [storage, setStorage] = useState([])
+  const [storage, setStorage] = useState(() => {
+    const storedArray = localStorage.getItem('myArray');
+    return storedArray ? JSON.parse(storedArray) : [];
+  });
   return (
     <div>GetLocal</div>
   )
